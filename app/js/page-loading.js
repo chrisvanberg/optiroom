0429f45e6d5c37e02be30ba0fe90a28808b1436f
Christophe Van Waesberghe <contact@chrisv.be>
2017-10-05 10:15:25 +0200

    Revert "Revert "Revert "Url avec angularJS"""
    This reverts commit 16dfb5344a74f05afdc0c591547edf8b495540be.

--- app/js/page-loading.js
+++ app/js/page-loading.js
@@ -1,4 +1,4 @@
-/*var lastLoaded;
+var lastLoaded;
 function loadPage(page){
 
     $("#content").load(page+'.html');
@@ -15,38 +15,4 @@ function loadPage(page){
 }
 function loadSubPage(page){
     $("#content").load(page+'.html');
-
-}*/
-var lastLoaded;
-
-$(document).ready(function() {
-    $(".mainElement").bind("click", function () {
-        var cat = "."+(this.classList[1]);
-        if(cat != lastLoaded){
-            $(".subElement").slideUp(400);
-        }
-        $(".subElement"+cat).slideDown(200, function () {
-            $(".subElement"+cat).css("display", "table");
-        });
-
-    });
-});
-var app = angular.module("optiroom", ["ngRoute"]);
-app.config(function($routeProvider) {
-    $routeProvider
-        .when("/", {
-            templateUrl : "overview.html"
-        })
-        .when("/rooms", {
-            templateUrl : "rooms.html"
-        })
-        .when("/book", {
-            templateUrl : "book.html"
-        })
-        .when("/my-bookings",{
-            templateUrl : "my-bookings.html"
-        })
-        .when("/management",{
-            templateUrl : "management.html"
-        });
-});
+}
