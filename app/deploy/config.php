8a9cd05737e4f72d5d77d7f265967b912a8ec242
Christophe Van Waesberghe <contact@chrisv.be>
2017-10-05 10:59:20 +0200

    Lof file location update

--- app/deploy/config.php
+++ app/deploy/config.php
@@ -3,6 +3,6 @@ define("TOKEN", "f76c9d689721f5235590f74ae19c6f42");                            
 define("REMOTE_REPOSITORY", "git@github.com:KeydownR/optiroom.git"); // The SSH URL to your repository
 define("DIR", "/var/www/optiroom/");                          // The path to your repostiroy; this must begin with a forward slash (/)
 define("BRANCH", "refs/heads/dev");                                 // The branch route
-define("LOGFILE", "deploy.log");                                       // The name of the file you want to log to.
+define("LOGFILE", "./deploy.log");                                       // The name of the file you want to log to.
 define("GIT", "/usr/bin/git");                                         // The path to the git executable
 define("AFTER_PULL", "");                                              // A command to execute after successfully pulling
