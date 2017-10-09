9086c7866d0f516e938f823316d7f99359516ff1
Christophe Van Waesberghe <contact@chrisv.be>
2017-10-09 12:54:36 +0200

    rooms/full implemented - error code implemented

--- opti_api/api.py
+++ opti_api/api.py
@@ -9,7 +9,7 @@ api = Api(app)
 @api.route('/system')
 class System(Resource):
     def get(self):
-        return {'state': 'up','version': '0.1.4', 'motd': 'N/A'}
+        return {'state': 'up','version': '0.1.5', 'motd': 'N/A'}
         
 @api.route('/motd')
 class System(Resource):
@@ -20,26 +20,32 @@ class System(Resource):
 class Rooms(Resource):
     def get(self):
            return {'A10': {'numFloor': '0','idBuilding': '1','typeRoom': '1'},'L04': {'numFloor': '0','idBuilding': '1','typeRoom': '2'},'L35': {'numFloor': '0','idBuilding': '1','typeRoom': '2'}}
+
+@api.route('/rooms/full')
+class Rooms(Resource):
+    def get(self):
+           return {'A10': {'numFloor': '0','idBuilding': '1','typeRoom': '1', 'opti_light': 'avaliable', 'opti_heat': 'avaliable', 'nbSeats': '250', 'projector' : 'true', 'soundSystem': 'true'},'L04': {'numFloor': '0','idBuilding': '1','typeRoom': '2', 'opti_light': 'false', 'opti_heat': 'avaliable', 'nbSeats': '70', 'projector' : 'true', 'soundSystem': 'false'},'L35': {'numFloor': '0','idBuilding': '1','typeRoom': '2', 'opti_light': 'avaliable', 'opti_heat': 'false', 'nbSeats': '40', 'projector' : 'false', 'soundSystem': 'true'}}
+
                      
 @api.route('/room/<string:room_id>')
 class Room(Resource):
     def get(self, room_id):
         if room_id == 'A10':
-            return {'A10': {'numFloor': '0','idBuilding': '1','typeRoom': '1'}}
+            return {'A10': {'numFloor': '0','idBuilding': '1','typeRoom': '1', 'opti_light': 'avaliable', 'opti_heat': 'avaliable', 'nbSeats': '250', 'projector' : 'true', 'soundSystem': 'true'}}
         elif room_id == 'L04':
-            return {'L04': {'numFloor': '0','idBuilding': '1', 'typeRoom': '2'}}
+            return {'L04': {'numFloor': '0','idBuilding': '1', 'typeRoom': '2', 'opti_light': 'false', 'opti_heat': 'avaliable', 'nbSeats': '70', 'projector' : 'true', 'soundSystem': 'false'}}
         else:
-        	return {'error':'Invalid room number'}
+        	return {'error':'Invalid room number'}, 404
 
 @api.route('/state/<string:room_id>')
 class Room(Resource):
     def get(self, room_id):
         if room_id == 'A10':
-            return {'state': 'busy'}
+            return {'A10': {'state': 'busy'}}
         elif room_id == 'L04':
-            return {'L04': {'numFloor': '0','idBuilding': '1', 'typeRoom': '2'}}
+            return {'L04': {'state': 'available'}}
         else:
-        	return {'error':'Invalid room number'}
+        	return {'error':'Invalid room number'}, 404
             
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
