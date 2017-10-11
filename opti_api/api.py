3c24101f2010ec009587c2fb30fb8ad9b0551739
Christophe Van Waesberghe <contact@chrisv.be>
2017-10-11 17:18:09 +0200

    API Linked with the BDD for /rooms

--- opti_api/api.py
+++ opti_api/api.py
@@ -1,32 +1,67 @@
 from flask import Flask
 from flask import abort
+from flask import jsonify
+from flask_mysqldb import MySQL
 from flask_restplus import Resource, Api
 import socket
 
 app = Flask(__name__)
 api = Api(app)
 
+mysql = MySQL()
+app.config['MYSQL_HOST'] = 'dev.optiroom.net'
+app.config['MYSQL_USER'] = 'opti_api'
+app.config['MYSQL_PASSWORD'] = 'YFdcxYJS:ng3PcvndfGeIeRxhuOYiP'
+app.config['MYSQL_DB'] = 'optiroom'
+mysql.init_app(app)
+
 @api.route('/system')
 class System(Resource):
     def get(self):
-        return {'state': 'up','version': '0.1.5', 'motd': 'N/A'}
-        
+        return {'state': 'up','version': '0.1.6', 'motd': 'N/A'}
+
+@api.route('/test')
+class System(Resource):
+    def get(self):
+
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM opti_building")
+        row = cur.fetchone()
+        if row:
+            return {'state': row[1]}
+        else:
+            return {'state': 'Username and/or password incorrect.'}
+
+
 @api.route('/motd')
 class System(Resource):
     def get(self):
         return {'motd': 'N/A'}
-        
+
 @api.route('/rooms')
 class Rooms(Resource):
     def get(self):
-           return {'A10': {'numFloor': '0','idBuilding': '1','typeRoom': '1'},'L04': {'numFloor': '0','idBuilding': '1','typeRoom': '2'},'L35': {'numFloor': '0','idBuilding': '1','typeRoom': '2'}}
+        rooms = []
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM get_rooms")
+        for row in cur:
+            room = {
+            'building_id': row[0],
+            'building_name': row[1],
+            'room_id': row[2],
+            'floorNum': row[3],
+            'roomType': row[4],
+            'roomDescription': row[5]}
+            rooms.append(room)
+        return jsonify(rooms)
+           #return {'A10': {'numFloor': '0','idBuilding': '1','typeRoom': '1'},'L04': {'numFloor': '0','idBuilding': '1','typeRoom': '2'},'L35': {'numFloor': '0','idBuilding': '1','typeRoom': '2'}}
 
 @api.route('/rooms/full')
 class Rooms(Resource):
     def get(self):
            return {'A10': {'numFloor': '0','idBuilding': '1','typeRoom': '1', 'opti_light': 'avaliable', 'opti_heat': 'avaliable', 'nbSeats': '250', 'projector' : 'true', 'soundSystem': 'true'},'L04': {'numFloor': '0','idBuilding': '1','typeRoom': '2', 'opti_light': 'false', 'opti_heat': 'avaliable', 'nbSeats': '70', 'projector' : 'true', 'soundSystem': 'false'},'L35': {'numFloor': '0','idBuilding': '1','typeRoom': '2', 'opti_light': 'avaliable', 'opti_heat': 'false', 'nbSeats': '40', 'projector' : 'false', 'soundSystem': 'true'}}
 
-                     
+
 @api.route('/room/<string:room_id>')
 class Room(Resource):
     def get(self, room_id):
@@ -46,6 +81,6 @@ class Room(Resource):
             return {'L04': {'state': 'available'}}
         else:
         	return {'error':'Invalid room number'}, 404
-            
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
