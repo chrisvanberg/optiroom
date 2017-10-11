e925d70d91540118dc30a147a831fa765d8a8d63
DamboisP <pierredambois@gmail.com>
2017-10-11 21:43:40 +0200

    Favicon

--- app/user-interface.php
+++ app/user-interface.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!--<link rel="icon" type="image/jpg" href="img/logo.jpg" />-->
+    <link rel="icon" type="image/png" href="img/favicon.png" />
     <link rel="stylesheet" href="css/UI.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
