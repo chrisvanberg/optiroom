26f2061316d653981a92c9238948f2d0e1403ce4
Christophe Van Waesberghe <contact@chrisv.be>
2017-11-27 18:21:28 +0100

    JWT & /user/workspaces fully implemented

--- opti_api/opti_api-run.sh
+++ opti_api/opti_api-run.sh
@@ -7,4 +7,4 @@ clear
 docker build -t  opti_api .
 clear
 echo "Build finished, launching the container"
-docker run --name opti_api -e JWT_SECRET_KEY="optiroom" -e MYSQL_HOST="dev.optiroom.net" -e MYSQL_USER="opti_api" -e MYSQL_PASSWORD="YFdcxYJS:ng3PcvndfGeIeRxhuOYiP" -e MYSQL_DB="optiroom" -e DEBUG="True" -p 5000:5000 opti_api
+docker run --name opti_api -e JWT_SECRET_KEY="TpZueWqbPLBPtTCPDYUZqXAJ944EbdZFaconlEvX9VBcYFa8hcmYGmK4RSOoCgBrZ5Y/T7P/uShrEpktgQ1fEXFrqQFnEq2am1M0" -e MYSQL_HOST="dev.optiroom.net" -e MYSQL_USER="opti_api" -e MYSQL_PASSWORD="YFdcxYJS:ng3PcvndfGeIeRxhuOYiP" -e MYSQL_DB="optiroom" -e DEBUG="True" -p 5000:5000 opti_api
